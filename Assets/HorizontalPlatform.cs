6f428ed6007ac64a600d611cc3064caffdb87718
Lövlie <fredriklovlie@gmail.com>
2020-03-13 13:44:56 +0100

    Constantly Moving Platforms

---
+++ Assets/HorizontalPlatform.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HorizontalPlatform : MonoBehaviour
+{
+    [SerializeField] private float MovementSpeed = 5.0f;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        transform.position -= new Vector3(MovementSpeed * Time.deltaTime, 0, 0);
+    }
+}
